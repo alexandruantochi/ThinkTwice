017eccdb1de9d4cb782ddf5dcfa364fd5f2e9fb7
alexandru_poputoaia <kuscog@gmail.com>
2017-04-24 14:33:45 +0300

    GuestPage aka about page
    User does not have to be authenticated anymore to se the app

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,22 +6,6 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
     public function products(){
         return view('products');
     }

dffbd07028aea6bd5856764d7eccee9e15f34064
alexandru.antochi <alexandru.antochi@gmail.com>
2017-04-26 00:44:39 +0300

    added contact form

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact.contact');
+    }
+
+    public function submit()
+    {
+        echo 'Submitted';
+    }
+
+}

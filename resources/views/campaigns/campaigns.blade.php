673fd7eb28ea72147ce0434d7c26126cbcc7f99e
Arhire Ionut <ionutarhire20@gmail.com>
2017-04-25 22:24:51 +0300

    modularized the code; includeble view for printing a grid of results

--- resources/views/campaigns/campaigns.blade.php
+++ resources/views/campaigns/campaigns.blade.php
@@ -91,36 +91,7 @@
 
             <div class="container" align="center" style="padding-bottom: 5em">
 
-                @php ($counter = 0)
-                @php ($perRow = 3)
-
-                @foreach($default_campaigns as $key => $value)
-
-                    @if ($counter % $perRow == 0)
-                        <div class="row">
-                    @endif
-
-                    <div class="col-sm-12 col-md-4">
-                        <div class="well">
-                            <div class="card" style="width: 20rem;">
-                                <a href="/campaign/{{ $key }}" class="thumbnail">
-                                    <img class="card-img-top" src=" {{ asset('images/campaigns/' . $key . '/' . $key . '.jpg') }} " alt="{{ $key }}" style="width: 100%; height: 15em;  !important;" >
-                                </a>
-                                <div class="card-block" style="height:8em">
-                                    <h4 class="card-title"> {{$value[0]}} </h4>
-                                    <p class="card-text">{{$value[1]}}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    @if (($counter + 1) % $perRow == 0)
-                        </div>
-                    @endif
-
-                    @php ($counter = $counter + 1)
-
-                @endforeach
+                @include('layouts.ResultsGrid', array('perRow' => 3, 'type1' => "campaign", 'type2' => "campaigns", 'default_results' => $default_campaigns));
 
             </div>
 

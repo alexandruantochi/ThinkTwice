467c38e9ec4b796635c4d737ece301ad92e87da7
alexandru.antochi <alexandru.antochi@gmail.com>
2017-05-27 22:09:56 +0300

    basic controllers

---
+++ app/Http/Controllers/Companies/CompanyBrowserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Companies;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Company;
+
+class CompanyBrowserController extends Controller
+{-
+    public function browseCompanies(Request $request, $page=1)
+    {
+        $companies =  Company::orderBy('rating')->paginate(5);
+        return view('companies.browse', compact('companies'));
+    }
+}

254148341674d8d1184637dc156d8745b9b4d213
Arhire Ionut <ionutarhire20@gmail.com>
2017-05-27 18:31:39 +0300

    cleaned the code for (CampaignsPopularityController); added basic functionality to the CampaignsTrendingController

--- app/Http/Controllers/Campaign/CustomClasses/CampaignConfigurations.php
+++ app/Http/Controllers/Campaign/CustomClasses/CampaignConfigurations.php
@@ -11,11 +11,13 @@ final class CampaignConfigurations {
 
     public $num_default_results;
     public $num_results_page;
+    public $default_trending_period;
 
     private function __construct() {
 
         $this->num_default_results = 6;
         $this->num_results_page = 10;
+        $this->default_trending_period = 12;
 
     }
 

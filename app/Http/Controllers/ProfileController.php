6083dac9b86c661315984bfedf881a522361ecd3
fistinflame <silitradragos@yahoo.com>
2017-05-29 22:42:25 +0300

    Getting session ready for logged user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+
 class ProfileController extends Controller
 {
+
     public function showprofile()
     {
         return view('profile\profile');
